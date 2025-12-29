f7f44d1f55d63989cac07f737d3574ee079a0171
Ian Brown <ibapps39@gmail.com>
2025-12-29 15:26:53 -0700

    Test acceletration branch

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+typedef struct my_box
+{
+    Vector2 position;
+    Vector2 size;
+    Color color;
+    Vector2 speed;
+} my_box;
+
 int main(void)
 {
 
@@ -15,61 +23,51 @@ int main(void)
     Camera2D camera = (Camera2D){0};
     camera = init_cam(camera, SCREEN_CENTER);
 
-    Player player = init_player(
-        SCREEN_CENTER, DEFAULT_PLAYER_SIZE, DEFAULT_PLAYER_COLOR
-    );
+    my_box b = (my_box){
+        .position = (Vector2){.x = center_x, .y = center_y},
+        .size = (Vector2){.x = 10, .y = 35},
+        .color = RED,
+        .speed = (Vector2){.x = 0, .y = 0}};
 
-    Vector2 platform_pos = (Vector2){.x = center_x/3, .y = screen_bottom-screen_bottom/3};
-    Vector2 platform_size = (Vector2){.x = .0333*window_width*9, .y = .0667*window_height};
-    Rectangle platform_hitbox = (Rectangle){
-        .x = platform_pos.x, .y = platform_pos.y, 
-        .width = platform_size.x, .height = platform_size.y
-    };
-    Color platform_color = ORANGE;
+    my_box b1 = (my_box){
+        .position = (Vector2){.x = 0, .y = screen_bottom - center_y / 2},
+        .size = (Vector2){.x = 1000, .y = 10},
+        .color = YELLOW,
+        .speed = (Vector2){.x = 0, .y = 0}};
 
-    int check = 0;
+        float g = 1.0f;
+        float velocityY = 0.0f;
+        float jump_force = -12.0f;
 
     while (!WindowShouldClose())
     {
-        move_xz(&player.position, DEFAULT_HORIZONTAL_SPEED);
-        Gravity(&player.position);
-        contain_player_debug(&player, window_height);
-        if( check_hitbox(
-            &player.position, 
-            player.size.x, 
-            player.size.y, 
-            &platform_pos, 
-            platform_size.x, 
-            platform_size.y 
-        ))
+        Rectangle br = get_hitbox(&b.position, b.size.x, b.size.y);
+        Rectangle br1 = (Rectangle){ .x = b1.position.x, .y = b1.position.y, .width = b1.size.x, .height = b1.size.y };
+        velocityY += g;
+        b.position.y += velocityY;
+
+        if (CheckCollisionRecs(br, br1) && velocityY > 0)
         {
-            bounce(&player.position, .0333f*screen_bottom);
+            b.position.y = br1.y - b.size.y;
+            velocityY = 0.0f;
+            velocityY = jump_force;
         }
+        
+        if (b.position.y > screen_bottom) {
+            b.position.y = 0;
+            velocityY = 0; // It's okay to reset velocity here because the player "spawned"
+        }
+
         BeginDrawing();
         ClearBackground(BLACK);
         BeginMode2D(camera);
-        //draw_platforms();
-        draw_player(&player);
-        DrawRectangleV(platform_pos, platform_size, platform_color);
 
-        //DrawBoundingBox(player.hitbox, GREEN);
+        DrawRectangle(b.position.x, b.position.y, b.size.x, b.size.y, b.color);
+        DrawRectangle(b1.position.x, b1.position.y, b1.size.x, b1.size.y, b1.color);
 
         EndMode2D();
         // Draw text showing players position
-        DrawText(
-            TextFormat("Player position: (%.2f, %.2f)", player.position.x, player.position.y),
-            10,
-            10,
-            20,
-            WHITE
-        );
-                DrawText(
-            TextFormat("check_bounce: %i)", check),
-            40,
-            40,
-            20,
-            WHITE
-        );
+
         EndDrawing();
     }
     CloseWindow();
